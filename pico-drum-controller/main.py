fac5c5ed7831c102245e363b8c6c782ac816498a
lukecbennett <lukecbennett90@googlemail.com>
2025-07-16 23:58:20 +0100

    Update main.py

--- pico-drum-controller/main.py
+++ pico-drum-controller/main.py
@@ -4,3 +4,5 @@ from secrets import secrets
 ota_updater = OTAUpdater(secrets['ssid'], secrets['password'], secrets['url'], secrets['filename'])
 
 ota_updater.download_and_install_update_if_available()
+
+#did it work?
