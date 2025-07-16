6caf2ab38449c4fcb5ad18ae69e5baffa0bf8684
lukecbennett <lukecbennett90@googlemail.com>
2025-07-16 23:18:26 +0100

    Update main.py

--- pico-drum-controller/main.py
+++ pico-drum-controller/main.py
@@ -1,25 +1,6 @@
-from machine import Pin
-import time
+from ota import OTAUpdater
+from secrets import secrets
 
-# Pin Definitions
-RELAY_1_PIN = 9      # GPIO controlling Relay 1
-BUTTON_A_PIN = 12    # GPIO for Switch A (Button A)
+ota_updater = OTAUpdater(secrets['ssid'], secrets['password'], secrets['url'], secrets['filename'])
 
-# Set up the relay as an output
-relay1 = Pin(RELAY_1_PIN, Pin.OUT)
-relay1.value(0)  # Start with relay off
-
-# Set up the button as input with pull-up
-button_a = Pin(BUTTON_A_PIN, Pin.IN, Pin.PULL_UP)
-
-print("Press Button A to activate Relay 1")
-
-while True:
-    if button_a.value() == 0:  # Button A is pressed (active low)
-        print("Button A pressed – activating relay")
-        relay1.value(1)        # Turn relay on
-    else:
-        relay1.value(0)        # Turn relay off
-
-    time.sleep(0.1)
-
+ota_updater.download_and_install_update_if_available()
